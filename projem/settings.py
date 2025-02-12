1fee5f674ab09334d153f1f2e10fe525ee1cfc18
ykablan <ahmet.norpa@gmail.com>
2025-02-12 15:28:20 +0300

    allow host eklendi

--- projem/settings.py
+++ projem/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 #ALLOWED_HOSTS = []
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("norpa.com.tr","www.norpa.com.tr", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
